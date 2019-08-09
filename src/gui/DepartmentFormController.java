40e5480d2838ee939b6644c9b84a008e2fbbd025
WilkerLira <34160767+WilkerLira@users.noreply.github.com>
2019-08-09 20:01:43 -0300

    Classe ValidationException

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listener.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exception.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
@@ -65,6 +68,9 @@ public class DepartmentFormController implements Initializable{
 			notifyDataChangeListener();
 			Utils.currentStage(event).close();
 
+		}catch (ValidationException e) {
+			setErrorMessage(e.getErros());
+			
 		} catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
@@ -81,8 +87,18 @@ public class DepartmentFormController implements Initializable{
 	private Department getFormData() {
 		Department depart = new Department();
 
+		ValidationException exception = new ValidationException("Erro de validação");
+		
 		depart.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addErro("name", "Campo nome está vázio");
+		}
 		depart.setName(txtName.getText());
+		
+		if (exception.getErros().size() > 0) {
+			throw exception;
+		}
 
 		return depart;
 	}
@@ -108,6 +124,14 @@ public class DepartmentFormController implements Initializable{
 		}
 		txtId.setText(String.valueOf(department.getId()));
 		txtName.setText(department.getName());
+	}
+	
+	private void setErrorMessage(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if (fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
 	}
 
 }
